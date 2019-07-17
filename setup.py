7e06389eb6de1e4ec0f07ae855ea7951f76968bb
José Luis Rodríguez <cyb3rpandah@gmail.com>
2019-07-17 17:52:13 -0400

    File Updated
    Setup file

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup (
 	version = '1.0',
 	description = 'Python scripts to improve hunting campaigns',
 	author = 'Jose Rodriguez',
-	authorEmail = 'cyb3rpandah@gmail.com',
-	)
+	author_email = 'cyb3rpandah@gmail.com',
+	py_modules = ['HunterTools'])
 	
