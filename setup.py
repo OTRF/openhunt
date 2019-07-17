a1c1b6fb3bb80fd4b44e6a3be288996cb0519301
José Luis Rodríguez <cyb3rpandah@gmail.com>
2019-07-17 16:36:13 -0400

    File Updated
    Setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# Author: Jose Rodriguez (@Cyb3rPandaH)
+
+from setuptools import setup
+
+setup (
+	name = 'HunterTools',
+	version = '1.0',
+	description = 'Python scripts to improve hunting campaigns',
+	author = 'Jose Rodriguez',
+	authorEmail = 'cyb3rpandah@gmail.com',
+	)
+	
