284a9c6cd5c32bb546b2b4305333e612c531d03d
José Luis Rodríguez <cyb3rpandah@gmail.com>
2019-07-19 09:47:12 -0400

    Code Update
    Code update extract_nested_fields

--- openhunt/logparser.py
+++ openhunt/logparser.py
@@ -10,7 +10,9 @@ class winlogbeat(object):
 		#If beats version field is 7 or 8, do something
 		return
 	def extract_nested_fields(self, path):
-		mordorFile = pd.read_json(path, lines = True)
-		eventDataField = mordorFile['event_data'].apply(pd.Series)
-		mordorDf = pd.concat([mordorFile, eventDataField], axis = 1)
-		return mordorDf
+		mordor_file = pd.read_json(path, lines = True)
+		event_data_field = mordor_file['event_data'].apply(pd.Series)
+		mordor_file_wo_event_data = mordor_file.drop('event_data', axis = 1)
+		df = pd.concat([mordor_file_wo_event_data, event_data_field], axis = 1)
+		mordor_df= df.dropna(axis = 1,how = 'all')
+		return mordor_df
