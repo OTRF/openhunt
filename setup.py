14b559f0e91076b6f8673178a9e60a6621d4bd51
José Luis Rodríguez <cyb3rpandah@gmail.com>
2019-08-13 10:06:40 -0400

    Setup file update
    code update

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setuptools.setup (
 	name = 'openhunt',
 	version = '1.0',
 	author = 'Jose Rodriguez',
-	author_email = 'cyb3rpandah@gmail.com',
 	description = 'Python scripts to improve hunting campaigns',
 	long_description=long_description,
 	long_description_content_type="text/markdown",
@@ -27,11 +26,11 @@ setuptools.setup (
         'Development Status :: 4 - Beta',
         'Operating System :: OS Independent',
         'Topic :: Security',
-        'OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
     ]
 )
